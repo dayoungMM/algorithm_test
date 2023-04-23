373a2827d2b478518a746bed16f518cf2b7dd2bc
dayoungMM <dalbeetm@gmail.com>
2023-04-23 15:09:11 +0900

    최단거리

---
+++ djikstra/future.py
@@ -0,0 +1,38 @@
+INF = int(1e9)
+
+
+n, m = map(int, input().split())
+
+graph = [[INF] * (n + 1) for _ in range(n + 1)]
+
+# 자기 자신에서 자기 자신으로 가는 비용은 0으로 초기화
+for a in range(1, n + 1):
+    for b in range(1, n + 1):
+        if a == b:
+            graph[a][b] = 0
+
+# 각 간선에 대한 정보를 입력 받아, 그 값으로 초기화
+for _ in range(m):
+    # A와 B가 서로에게 가는 비용은 1이라고 설정
+    a, b = map(int, input().split())
+    graph[a][b] = 1
+    graph[b][a] = 1
+
+# 거쳐 갈 노드 X와 최종 목적지 노드 K를 입력받기
+x, k = map(int, input().split())
+
+# 플로이드 워셜 알고리즘
+for k in range(1, n + 1):
+    for a in range(1, n + 1):
+        for b in range(1, n + 1):
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+
+
+distance = graph[1][k] + graph[k][x]
+
+# 도달할 수 없는 경우, -1을 출력
+if distance >= 1e9:
+    print("-1")
+# 도달할 수 있다면, 최단 거리를 출력
+else:
+    print(distance)
